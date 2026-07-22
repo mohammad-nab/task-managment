c3b9f705e542275df22554222b878ebe9c690054
Mohammad <mohammadnabavi1384@gmail.com>
2026-07-22 18:19:11 +0330

    get task router

--- database.py
+++ database.py
@@ -5,3 +5,10 @@ from sqlalchemy.orm import sessionmaker
 engine = create_engine("sqlite:///tasks.db", echo=True)
 
 SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+from fastapi import Depends
+import models
+from database import get_db
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/task")
+def get_task(db: Session = Depends(get_db)):
+    result = db.scalars(select(models.Task)).all()
+    return result
